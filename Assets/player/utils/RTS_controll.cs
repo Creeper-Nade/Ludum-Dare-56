dbde1f54908ae12cfc048b0f968f56d4a2475b4c
Creeper Nade <146449575+Creeper-Nade@users.noreply.github.com>
2024-10-07 04:08:22 +0800

    ADDED SELECTING MECHANIC

--- Assets/player/utils/RTS_controll.cs
+++ Assets/player/utils/RTS_controll.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 using CodeMonkey.Utils;
 public class RTS_controll : MonoBehaviour
 {
-    private Vector3 startPos;
-    private List<UnitRTS> selectedUnitRTS;
+    [SerializeField] private Transform selectionAreaTransform;
+    public Vector3 startPos;
+    [SerializeField]public List<UnitRTS> selectedUnitRTS;
     
     private void Awake() {
         selectedUnitRTS=new List<UnitRTS>();
+        selectionAreaTransform.gameObject.SetActive(false);
     }
     private void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
+            selectionAreaTransform.gameObject.SetActive(true);
             startPos=UtilsClass.GetMouseWorldPosition();
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 currentMousePostition=UtilsClass.GetMouseWorldPosition();
+            Vector3 lowerLeft = new Vector3(
+                Mathf.Min(startPos.x,currentMousePostition.x),
+                Mathf.Min(startPos.y,currentMousePostition.y)
+            );
+            Vector3 upperRight=new Vector3(
+                Mathf.Max(startPos.x,currentMousePostition.x),
+                Mathf.Max(startPos.y,currentMousePostition.y)
+            );
+            selectionAreaTransform.position=lowerLeft;
+            selectionAreaTransform.localScale=upperRight-lowerLeft;
         }
 
         if(Input.GetMouseButtonUp(0))
         {
+            selectionAreaTransform.gameObject.SetActive(false);
             Collider2D[] collider2DArray=Physics2D.OverlapAreaAll(startPos,UtilsClass.GetMouseWorldPosition());
             //deselect
             foreach(UnitRTS unitRTS in selectedUnitRTS)
